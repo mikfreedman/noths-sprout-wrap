4090068a3e6fb9e94ab3bfa0ea9801efaad07ddc
pivotal <pivotal@tbolt-02.lon.pivotallabs.com>
2014-03-05 15:31:04 +0000

    Added mysql config for faster dev setup and correct socket path

--- site-cookbooks/noths/recipes/mysql.rb
+++ site-cookbooks/noths/recipes/mysql.rb
@@ -1,9 +1,8 @@
 #http://solutions.treypiepmeier.com/2010/02/28/installing-mysql-on-snow-leopard-using-homebrew/
 require 'pathname'
 
-link "/tmp/mysql.sock" do
-  to "/var/lib/mysql/mysql.sock"
-  owner node['current_user']
+template "/etc/my.cnf" do
+  source "mysql/my.cnf"
 end
 
 include_recipe "pivotal_workstation::mysql"
