fa924b180709c61edb2b340560cfcab08aa5354d
Alex Sunderland and Michael Freedman <alexsunderland+mfreedman@notonthehighstreet.com>
2014-03-07 17:42:31 +0000

    add the other projects to git

--- site-cookbooks/noths/recipes/notonthehighstreet_repo.rb
+++ site-cookbooks/noths/recipes/notonthehighstreet_repo.rb
@@ -1,6 +1,9 @@
-git "checkout notonthehighstreet" do
-  destination File.expand_path("~/workspace/notonthehighstreet")
-  repository  "git@github.com:notonthehighstreet/noths-sprout-wrap.git"
-  revision    "integration"
-  action      :checkout
+node['git_projects'].each do |address|
+  git "checkout #{address.split("/").last}" do
+    destination File.expand_path("~/workspace")
+    repository address
+    revision    "master"
+    action      :checkout
+    user node["current_user"]
+  end
 end
