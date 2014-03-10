30c40d0925631b098936d912ddf70adf59dd3ed0
Michael Freedman and Theo Cushion <mfreedman+tcushion@notonthehighstreet.com>
2014-03-10 13:10:17 +0000

    install sublime packages

--- site-cookbooks/noths/recipes/sublime_text.rb
+++ site-cookbooks/noths/recipes/sublime_text.rb
@@ -1,13 +1,13 @@
 include_recipe "sprout-osx-apps::sublime_text"
 
-application_support = File.expand_path("~/Library/Application Support/Sublime Text 2/Packages/User")
+# application_support = File.expand_path("#{node['sprout']['home']}/Library/Application Support/Sublime Text 2/Packages/User")
 
-remote_file "sublime filename_to_clipboard" do
-  source "https://raw.github.com/dougdroper/filename_to_clipboard/master/filename_to_clipboard_command.py"
-  path   File.join(application_support, "filename_to_clipboard_command.py")
-end
+# remote_file "sublime filename_to_clipboard" do
+  # source "https://raw.github.com/dougdroper/filename_to_clipboard/master/filename_to_clipboard_command.py"
+  # path   File.join(application_support, "filename_to_clipboard_command.py")
+# end
 
-template "sublime_text key mappings" do
-  source "sublime_text/Default (OSX).sublime-keymap"
-  path   File.join(application_support, "Default (OSX).sublime-keymap")
-end
+# template "sublime_text key mappings" do
+  # source "sublime_text/Default (OSX).sublime-keymap"
+  # path   File.join(application_support, "Default (OSX).sublime-keymap")
+# end
