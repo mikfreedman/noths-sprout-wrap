af7ee683560a8b35d6a8a0d06ee934ded08c1bc0
Pivotal <pivotal@guest-9-30.lon.pivotallabs.com>
2014-03-10 19:18:20 +0000

    Disabled timeout for loading databases

--- site-cookbooks/noths/recipes/database_dumps.rb
+++ site-cookbooks/noths/recipes/database_dumps.rb
@@ -2,30 +2,31 @@ db_dump_dir = File.expand_path("#{node[:sprout][:home]}/Downloads")
 db_user     = "root"
 
 [
-	{
-		:name   => "noths_uk",
-		:source => ["http://dumps.hq.noths.com/mysql.sql.gz", "https://dumps.hq.noths.com/mysql.sql.gz"],
-		:path   => File.join(db_dump_dir, "mysql_uk.sql.gz")
-	},
-	{
-		:name   => "noths_de",
-		:source => ["http://dumps.hq.noths.com/mysql_de.sql.gz", "https://dumps.hq.noths.com/mysql_de.sql.gz"],
-		:path   => File.join(db_dump_dir, "mysql_de.sql.gz")
-	}
+  {
+    :name   => "noths_uk",
+    :source => ["http://dumps.hq.noths.com/mysql.sql.gz", "https://dumps.hq.noths.com/mysql.sql.gz"],
+    :path   => File.join(db_dump_dir, "mysql_uk.sql.gz")
+  },
+  {
+    :name   => "noths_de",
+    :source => ["http://dumps.hq.noths.com/mysql_de.sql.gz", "https://dumps.hq.noths.com/mysql_de.sql.gz"],
+    :path   => File.join(db_dump_dir, "mysql_de.sql.gz")
+  }
 ].each do |details|
 
-	remote_file "download #{details[:name]}" do
-	  path   details[:path]
-	  source details[:source]
-	  action :create_if_missing
-	end
+  remote_file "download #{details[:name]}" do
+    path   details[:path]
+    source details[:source]
+    action :create_if_missing
+  end
 
-	bash "load #{details[:name]}" do
-	  code <<-BASH 
-	    mysql -u#{db_user} -e 'CREATE DATABASE #{details[:name]};' && gzcat #{details[:path]} | mysql -u #{db_user} #{details[:name]} 
-	  BASH
-	  not_if  "mysql -u #{db_user} #{details[:name]} -e exit;"
-	end
+  bash "load #{details[:name]}" do
+    code <<-BASH 
+      mysql -u#{db_user} -e 'CREATE DATABASE #{details[:name]};' && gzcat #{details[:path]} | mysql -u #{db_user} #{details[:name]} 
+    BASH
+    timeout 60 * 60 * 24 * 2 # Wait 2 days, that should be enough
+    not_if  "mysql -u #{db_user} #{details[:name]} -e exit;"
+  end
 
 end
 
