10b4839383de349570c968fa4ee54caad3deb185
Alex Sunderland <AlexS@AlexS.local>
2014-03-12 11:12:39 +0000

    WIP

---
+++ site-cookbooks/noths/recipes/quicksilver.rb
@@ -0,0 +1,5 @@
+dmg_package "Quicksilver" do
+  source "http://qs0.qsapp.com/plugins/download.php"
+  checksum "7c2ebec0d478d711859991991a65e4deced9f9521ac555c07b6be42999504535"
+  action :install
+end
